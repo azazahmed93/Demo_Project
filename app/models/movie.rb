7ba02cdc8e3421fa99e20b8d13cab405586e98d2
azazahmed93 <azaz.nuces@gmail.com>
2017-07-20 16:32:58 +0500

    Actor: Adding actors with nested_form implemented.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,11 @@
 class Movie < ActiveRecord::Base
 	has_many :posters, dependent: :destroy
-
+	has_many :appearances
+	has_many :actors , through: :appearances, dependent: :destroy
 	def code
   		self.url.split('/').last if self.url
 	end
 	accepts_nested_attributes_for :posters
+	accepts_nested_attributes_for :actors
+
 end
