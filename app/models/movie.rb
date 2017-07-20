0afd4e8cdb700662237eeb8a9605f517d9691508
azazahmed93 <azaz.nuces@gmail.com>
2017-07-20 10:32:56 +0500

    Associations: added respective associations to models Movie and User.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ActiveRecord::Base
 	has_many :posters
-
+	has_many :favorites
+	has_many :users, through: :favorites
+	has_many :actors, through: :appearances 
 	def code
   		self.url.split('/').last if self.url
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  
+  has_many :favorites
+  has_many :movies, through: :favorites
   has_attached_file :avatar , styles: { medium: "300x300>", thumb: "100x100>" }, 
   									 default_url: "/app/assets/images/images.png"
-  
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable  
 end
