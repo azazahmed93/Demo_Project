786f58d43a4b54d2683b86aa2614df6a4bd6ee8a
azazahmed93 <azaz.nuces@gmail.com>
2017-07-21 12:59:38 +0500

    Posters: Added destroy_tru for individual poster deletion.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,8 +3,8 @@ class Movie < ActiveRecord::Base
 	has_many :posters, dependent: :destroy
 	has_many :appearances
 	has_many :actors , through: :appearances, dependent: :destroy
-	accepts_nested_attributes_for :posters
-	accepts_nested_attributes_for :actors
+	accepts_nested_attributes_for :posters, allow_destroy: true
+	accepts_nested_attributes_for :actors, allow_destroy: true
 
 	def code
   		self.url.split('/').last if self.url
