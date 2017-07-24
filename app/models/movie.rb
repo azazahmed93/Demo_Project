29657f939973136da65d20e8071e711a76c7bd51
azazahmed93 <azaz.nuces@gmail.com>
2017-07-24 16:03:26 +0500

    Navbar: profile picture and dropdown for logout.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ActiveRecord::Base
-	has_many :posters, dependent: :destroy
-	has_many :appearances
-	has_many :actors , through: :appearances, dependent: :destroy
-	accepts_nested_attributes_for :posters, allow_destroy: true
-	accepts_nested_attributes_for :actors, allow_destroy: true
+  has_many :posters, dependent: :destroy
+  has_many :appearances
+  has_many :actors , through: :appearances, dependent: :destroy
+  accepts_nested_attributes_for :posters, allow_destroy: true
+  accepts_nested_attributes_for :actors, allow_destroy: true
 
-	def code
-  		self.url.split('/').last if self.url
-	end
+  def code
+    self.url.split('/').last if self.url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :favorites
   has_many :movies, through: :favorites
-  has_attached_file :avatar , styles: { medium: "300x300>", thumb: "100x100>" }, 
+  has_attached_file :avatar , styles: { medium: "300x300>", thumb: "100x100>" , small: "50x50>" },
   									:default_url => "/images/2.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   devise :database_authenticatable, :registerable,
