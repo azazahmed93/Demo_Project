d9109930a724e572a4a81f6776be00ed72d56a63
azazahmed93 <azaz.nuces@gmail.com>
2017-07-24 12:39:31 +0500

    Confirmable: Added confirmable and configured with model user.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
 class Admin < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   resources :movies
   get 'users/all'
   root 'movies#index'
