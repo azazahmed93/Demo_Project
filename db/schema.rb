5258b0597464a17d866163e15a198a241496865f
azazahmed93 <azaz.nuces@gmail.com>
2017-07-28 13:11:54 +0500

    Report: Generated model report with references user and review.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725101004) do
+ActiveRecord::Schema.define(version: 20170728080847) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -121,6 +121,17 @@ ActiveRecord::Schema.define(version: 20170725101004) do
 
   add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
 
+  create_table "reports", force: :cascade do |t|
+    t.integer  "count",      limit: 4
+    t.integer  "user_id",    limit: 4
+    t.integer  "review_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "reports", ["review_id"], name: "index_reports_on_review_id", using: :btree
+  add_index "reports", ["user_id"], name: "index_reports_on_user_id", using: :btree
+
   create_table "reviews", force: :cascade do |t|
     t.text     "content",    limit: 65535
     t.integer  "user_id",    limit: 4
@@ -165,6 +176,8 @@ ActiveRecord::Schema.define(version: 20170725101004) do
   add_foreign_key "favorites", "movies"
   add_foreign_key "favorites", "users"
   add_foreign_key "posters", "movies"
+  add_foreign_key "reports", "reviews"
+  add_foreign_key "reports", "users"
   add_foreign_key "reviews", "movies"
   add_foreign_key "reviews", "users"
 end
